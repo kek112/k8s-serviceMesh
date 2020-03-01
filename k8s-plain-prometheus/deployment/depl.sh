9b3c49877771a3ebfa2f0c448c99aec5ac98cc27
Karl-Erik Kley <kkley@inoxio.de>
2020-03-01 19:42:05 +0100

    Wait for Pods to be ready

--- k8s-plain-prometheus/deployment/depl.sh
+++ k8s-plain-prometheus/deployment/depl.sh
@@ -1,2 +1,5 @@
 kubectl apply -f ./
+while [[ $(kubectl get pods -l app=hello -n test -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != *"True" ]]; do echo "waiting for pod" && sleep 1; done
+while [[ $(kubectl get pods -l app=world -n test -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != *"True" ]]; do echo "waiting for pod" && sleep 1; done
+while [[ $(kubectl get pods -l app=world2 -n test -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != *"True" ]]; do echo "waiting for pod" && sleep 1; done
 kubectl --namespace test port-forward svc/hello 30000

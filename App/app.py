0fd0a14c2d92dfe91780acd71c23277b0123fcd5
Karl-Erik Kley <kkley@inoxio.de>
2020-03-08 11:01:13 +0100

    Let flask app fail 20% of the time
    add comments

--- App/app.py
+++ App/app.py
@@ -1,19 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 import os
 import json
 import requests
+import random
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def returnText():
-
+    # fail 20% of the requests
+    if random.randint(0,100) < 20:
+        abort(404)
+    # if text env is not set return phrase
     if 'TEXT' not in os.environ:
         return 'Please specify env TEXT'
     else:
+        # check if next url is given for multi dependency
         if 'NEXT' not in os.environ:
             return os.environ['TEXT']
         else:
+            # send request to next url
             r = requests.get(os.environ['NEXT'])
             return os.environ['TEXT'] + ' ' + r.text
 

ebfa01a5114e4bd686fa9a7735c80a42e8ef221a
Karl-Erik Kley <kkley@inoxio.de>
2020-05-09 15:55:31 +0200

    Cleanup, new configs, removed not needed helm charts
    Fix faulty health endpoint

--- k8s-maesh/install.sh
+++ k8s-maesh/install.sh
@@ -1,7 +1,8 @@
 kubectl create namespace maesh
-helm install maesh maesh-master/helm/chart/maesh --namespace maesh --set controller.image.pullPolicy=IfNotPresent --set controller.image.tag=latest
-kubectl wait --for=condition=Ready pods --all -n maesh --timeout=500s
-#cd deployment
-#./depl.sh
+helm install --namespace=maesh maesh maesh/maesh --set kubedns=true
 
-#kubectl --namespace maesh port-forward svc/grafana 3000
+kubectl wait --for=condition=Ready pods --all -n maesh --timeout=500s
+cd deployment
+./depl.sh
+
+

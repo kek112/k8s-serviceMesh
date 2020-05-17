6698b78ea7727a2466a05e2c2c28cada71e2bcfa
Karl-Erik Kley <kkley@inoxio.de>
2020-05-17 11:51:45 +0200

    Wait for pods to be ready

--- k8s-plain-prometheus/install.sh
+++ k8s-plain-prometheus/install.sh
@@ -1,5 +1,6 @@
 kubectl create ns monitoring
 helm install metrics -n monitoring stable/prometheus-operator --set alertmanager.enabled=false
+kubectl wait --for=condition=Ready pods --all -n monitoring --timeout=500s
 
 # install LoadBalancer, Ingress Controller and deployment
 cd deployment || exit

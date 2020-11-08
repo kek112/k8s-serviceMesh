0e731f2cb70fc2191e67086cf6495aa6d734fff3
Karl-Erik Kley <karlskarls@web.de>
2020-11-08 09:26:09 +0100

    Remove not needed kubedne parameter

--- k8s-maesh/install.sh
+++ k8s-maesh/install.sh
@@ -1,5 +1,5 @@
 kubectl create namespace maesh
-helm install --namespace=maesh maesh maesh/maesh --set kubedns=true
+helm install --namespace=maesh maesh maesh/maesh
 
 kubectl wait --for=condition=Ready pods --all -n maesh --timeout=500s
 cd deployment
